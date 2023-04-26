e9b10bb9b66353bf1baf39eb3961c2bd142b7899
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-04-26 08:05:07 +0330

    Update OwnerService.java

--- data/src/main/java/com/autotradingwiz/animalclinicmanagement/services/OwnerService.java
+++ data/src/main/java/com/autotradingwiz/animalclinicmanagement/services/OwnerService.java
@@ -1,5 +1,6 @@
 package com.autotradingwiz.animalclinicmanagement.services;
 
+
 import com.autotradingwiz.animalclinicmanagement.models.Owner;
 
 public interface OwnerService extends CrudService<Owner, Long> {
