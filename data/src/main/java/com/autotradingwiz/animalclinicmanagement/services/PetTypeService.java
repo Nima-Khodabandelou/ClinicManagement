2abacdd937b300f7670d4e026629662d09624284
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-04-26 08:04:41 +0330

    Update PetTypeService.java

--- data/src/main/java/com/autotradingwiz/animalclinicmanagement/services/PetTypeService.java
+++ data/src/main/java/com/autotradingwiz/animalclinicmanagement/services/PetTypeService.java
@@ -1,5 +1,6 @@
 package com.autotradingwiz.animalclinicmanagement.services;
 
+
 import com.autotradingwiz.animalclinicmanagement.models.PetType;
 
 public interface PetTypeService extends CrudService<PetType, Long> {
