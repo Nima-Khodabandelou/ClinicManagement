6f0bb76258d60d7e1cdf478f2b0ccaa41525223a
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-05-17 16:22:24 +0330

    Update IndexController.java

--- web/src/main/java/com/autotradingwiz/animalclinicmanagement/controllers/IndexController.java
+++ web/src/main/java/com/autotradingwiz/animalclinicmanagement/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.autotradingwiz.animalclinicmanagement.controllers;
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
