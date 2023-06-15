6a2a8fe82c6d9d4761c380011589e8863f0abdfc
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-06-15 17:31:18 +0330

    Update SpecialityService.java

--- data/src/main/java/com/autotradingwiz/animalclinicmanagement/services/SpecialityService.java
+++ data/src/main/java/com/autotradingwiz/animalclinicmanagement/services/SpecialityService.java
@@ -1,6 +1,4 @@
 package com.autotradingwiz.animalclinicmanagement.services;
-
-
 import com.autotradingwiz.animalclinicmanagement.models.Speciality;
 
 public interface SpecialityService extends CrudService<Speciality, Long> {
